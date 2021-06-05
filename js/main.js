01f42b7d90e94b4dc3e03ac1b038b2f6abd78642
AndreyCher <forward14@bk.ru>
2021-06-05 10:50:12 +0300

    Исправил ошибки автопроверки

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const getRandomInteger = function(min, max) {
   const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
-}
+};
 
 const checkingStringlength = function(str, max) {
   return str.length <= max;
-}
+};
